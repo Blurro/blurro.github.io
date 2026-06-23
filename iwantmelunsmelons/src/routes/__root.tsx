b861ba166fa3ffffa324877915b8d8ebc1b09a07
Blurro <theblurofblue@gmail.com>
2026-06-23 21:57:31 +0100

    cooools

--- iwantmelunsmelons/src/routes/__root.tsx
+++ iwantmelunsmelons/src/routes/__root.tsx
@@ -6,7 +6,8 @@ export const Route = createRootRoute({
     meta: [
       { charSet: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { title: 'Happy Birthday Meluh!' },
+      { title: 'Happy Birthday mai Melun!' },
+      { name: 'darkreader-lock' },
     ],
     links: [
       {
